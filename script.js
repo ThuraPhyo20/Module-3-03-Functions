2df65a47913196ffe300d4a0c79c2bfd1e558c55
ThuRaPhyo <thuyaphyo7877@gmail.com>
2024-05-02 13:52:19 +0630

    solved

---
+++ script.js
@@ -0,0 +1,13 @@
+// TODO: Write Your JavaScript Code Here
+function check(x,y){
+    if(x===y){
+        console.log("They are equal in type and value");
+    }
+    else if(x==y){
+        console.log("They are equal in value");
+    }
+    else{
+        console.log("The values are not equal");
+    }
+}
+check(10,"p");
